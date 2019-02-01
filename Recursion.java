99587767c75f499d2e49f44c5dd2c7a50febb71b
lilkho <mkhondker@stuy.edu>
2019-01-31 20:45:53 -0500

    fib done

--- Recursion.java
+++ Recursion.java
@@ -10,7 +10,7 @@ public class Recursion{
       if (Math.abs((guess*guess-n)/(n)) <= tolerance)
         return guess;
       else
-        return sqrt(n, (n/guess + guess) / 2.0);
+        return sqrt(n, (n/guess + guess) / 2.0, tolerance);
     }
     public static double sqrt(double n, double tolerance){
       return sqrt(n,n,tolerance);
@@ -19,24 +19,29 @@ public class Recursion{
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
-     /
+     */
     private static int fib(int n, int first, int second) {
-      if (n==1||n==2) return 1;
+      if (n==1||n==2) return second;
       if (n<1) return 0;
       else
-        return fib(first+second,n-1,n-2);
+        return fib(n-1,second,first+second);
     }
     public static int fib(int n){
-
+      return fib(n,1,1);
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
+    public static ArrayList<Integer> makeAllSums(int n){
+      ArrayList<Integer> sums = new ArrayList<Integer>();
 
+      return sums;
     }
 
     public static void main(String[] args) {
+      System.out.println(sqrt(64,.00001));
+      System.out.println(sqrt(64,.0000000001));
+      for (int i=0;i<10;i++)
+        System.out.println(fib(i));
 
     }
-
 }
