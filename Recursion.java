07dd79f5cd6e2f82b9f496fc91796afc9c186c62
lilkho <mkhondker@stuy.edu>
2019-01-31 19:00:53 -0500

    lol im lost

--- Recursion.java
+++ Recursion.java
@@ -19,12 +19,23 @@ public class Recursion{
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
-     */
+     /
+    private static int fib(int n, int first, int second) {
+      if (n==1||n==2) return 1;
+      if (n<1) return 0;
+      else
+        return fib(first+second,n-1,n-2);
+    }
     public static int fib(int n){
+
     }
 
     /*As Per classwork*/
     public static ArrayList<Integer> makeAllSums(){
+
+    }
+
+    public static void main(String[] args) {
 
     }
 
